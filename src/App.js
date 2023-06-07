5b915e570de5b454b67728abd894ffb836be9e33
git-savio1.domi <techsavvydominic3@gmail.com>
2023-06-07 23:49:25 +0300

    Connect Home component with App component for routing and rendering

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Footer from './components/Footer/Footer'
+import Home from './components/Home/Home'
 
 function App() {
   return (
     <div className="w-screen h-screen">
+      <Home/>
 
-      
+
 
       <Footer/>
     </div>
