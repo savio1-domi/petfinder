0b19e853e469eb330d41d3748771bd32df5f1696
git-savio1.domi <techsavvydominic3@gmail.com>
2023-06-08 00:21:37 +0300

    Add react-router-dom package for client-side routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import Footer from './components/Footer/Footer'
 import Home from './components/Home/Home'
 import Navbar from './components/Navbar/Navbar'
+import { BrowserRoutes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="w-screen h-screen">
       <Navbar/>
-      <Home/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+      
+      </Routes>
 
 
 
