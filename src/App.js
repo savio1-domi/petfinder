14990320a81821046f1a5bcc80aac2907c8db674
git-savio1.domi <techsavvydominic3@gmail.com>
2023-06-08 00:00:48 +0300

    Integrate Navbar component into App component for consistent navigation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Footer from './components/Footer/Footer'
 import Home from './components/Home/Home'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div className="w-screen h-screen">
+      <Navbar/>
       <Home/>
 
 
